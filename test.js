7bf66af5e570d33b825fe9f928a0bd84ddaa67bc
ANSH GOEL <goelansh2006@gmail.com>
2026-07-31 13:37:29 +0530

    Import & export features

--- test.js
+++ test.js
@@ -6,10 +6,10 @@ console.log("Sum=",sum(1135,265,2365,65));
 console.log("Sum=",sum(11,12,13,15,16,17));
 
 
-console.log("add=",sum(1));
-console.log("add=",sum(12,34));
-console.log("add=",sum(101,209,1325));
-console.log("add=",sum(1135,265,2365,65));
-console.log("add=",sum(11,12,13,15,16,17));
+console.log("add=",add(1));
+console.log("add=",add(12,34));
+console.log("add=",add(101,209,1325));
+console.log("add=",add(1135,265,2365,65));
+console.log("add=",add(11,12,13,15,16,17));
 
 //curly braces hai toh named export hai warna default export hai.
