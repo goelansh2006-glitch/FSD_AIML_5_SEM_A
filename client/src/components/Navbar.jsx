f5549c8c7924a1b9d1fff027a56dd2cb7f605de2
ANSH GOEL <goelansh2006@gmail.com>
2026-09-03 14:43:20 +0530

    npm

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+    <a>Home</a>
+    <a>My Cart</a>
+    <a>My Order</a>
+    <a>Settings</a>
+    <a>My Profile</a>
+    <a>Logout</a>      
+    </div>
+  )
+}
+
+export default Navbar
